a570223741b549d5b3445a02e4f12141839723b3
backtovedas <deku55k@gmail.com>
2026-03-23 12:09:38 +0000

    fixed ig not recognizing reels

--- reel60.sh
+++ reel60.sh
@@ -9,13 +9,16 @@ INPUT="$1"
 OUTPUT="${INPUT%.*}_reel60.mp4"
 
 ffmpeg -i "$INPUT" \
-  -vf "scale=1080:1920:force_original_aspect_ratio=increase,crop=1080:1920,fps=60,colorspace=all=bt709:iall=bt2020:fast=1" \
+  -vf "scale=1080:1920:force_original_aspect_ratio=increase,crop=1080:1920,fps=60" \
   -c:v libx264 \
-  -profile:v high \
+  -profile:v main \
+  -level: 4.2
   -preset slow \
   -crf 23 \
   -maxrate 8M -bufsize 16M \
   -pix_fmt yuv420p \
+  -color:primaries bt709 \
+  -color:trc bt709 \
   -c:a aac -b:a 128k -ar 44100 -ac 2 \
   -movflags +faststart \
   "$OUTPUT"
